5228b5d9bf79f343741345a2b784b660100d3172
Relievv Automation <relievvautomation@gmail.com>
2026-02-23 16:35:20 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,10 @@
+ // redirect links 
 console.log(new Date().getFullYear() + " with love and grateful Developer Vishal ❤️");
 
 // redirect links 
-function redirecturl(){
- function windowredirecturl(url , target){
-    window.location.href = url;
+function windowredirecturl(url, target){
     window.open(url, target);
 }
-}
-
-redirecturl();
 
 // redirect 
  
