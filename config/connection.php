877089374c1dfc9d5cabc470c140639de0e779c7
Relievv Automation <relievvautomation@gmail.com>
2026-03-24 13:28:58 +0530

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -12,19 +12,80 @@
 
 
 
+
 <?php
-// Local XAMPP defaults; Coolify/Docker can provide DB_* or MYSQL_* variables.
-$dbhost = getenv('DB_HOST') ?: (getenv('MYSQL_HOST') ?: 'localhost');
-$dbuser = getenv('DB_USER') ?: (getenv('MYSQL_USER') ?: 'root');
+// Supports both discrete env vars and URL-style vars from platforms like Coolify.
+$dbUrl = getenv('DATABASE_URL');
+if ($dbUrl === false || $dbUrl === '') {
+    $dbUrl = getenv('MYSQL_URL');
+}
+if ($dbUrl === false || $dbUrl === '') {
+    $dbUrl = getenv('DB_URL');
+}
+
+$urlParts = false;
+if ($dbUrl !== false && $dbUrl !== '') {
+    $urlParts = parse_url($dbUrl);
+    if ($urlParts !== false && isset($urlParts['path'])) {
+        $urlParts['path'] = ltrim($urlParts['path'], '/');
+    }
+}
+
+$dbhost = getenv('DB_HOST');
+if ($dbhost === false || $dbhost === '') {
+    $dbhost = getenv('MYSQL_HOST');
+}
+if (($dbhost === false || $dbhost === '') && $urlParts !== false && isset($urlParts['host'])) {
+    $dbhost = $urlParts['host'];
+}
+if ($dbhost === false || $dbhost === '') {
+    $dbhost = 'localhost';
+}
+
+$dbuser = getenv('DB_USER');
+if ($dbuser === false || $dbuser === '') {
+    $dbuser = getenv('MYSQL_USER');
+}
+if (($dbuser === false || $dbuser === '') && $urlParts !== false && isset($urlParts['user'])) {
+    $dbuser = $urlParts['user'];
+}
+if ($dbuser === false || $dbuser === '') {
+    $dbuser = 'root';
+}
+
 $dbpassword = getenv('DB_PASSWORD');
 if ($dbpassword === false) {
+    $dbpassword = getenv('DB_PASS');
+}
+if ($dbpassword === false) {
     $dbpassword = getenv('MYSQL_PASSWORD');
+}
+if (($dbpassword === false || $dbpassword === '') && $urlParts !== false && isset($urlParts['pass'])) {
+    $dbpassword = $urlParts['pass'];
 }
 if ($dbpassword === false) {
     $dbpassword = '';
 }
-$dbname = getenv('DB_NAME') ?: (getenv('MYSQL_DATABASE') ?: 'relievv');
-$dbport = (int) (getenv('DB_PORT') ?: (getenv('MYSQL_PORT') ?: 3306));
+
+$dbname = getenv('DB_NAME');
+if ($dbname === false || $dbname === '') {
+    $dbname = getenv('MYSQL_DATABASE');
+}
+if (($dbname === false || $dbname === '') && $urlParts !== false && !empty($urlParts['path'])) {
+    $dbname = $urlParts['path'];
+}
+if ($dbname === false || $dbname === '') {
+    $dbname = 'relievv';
+}
+
+$dbport = getenv('DB_PORT');
+if ($dbport === false || $dbport === '') {
+    $dbport = getenv('MYSQL_PORT');
+}
+if (($dbport === false || $dbport === '') && $urlParts !== false && isset($urlParts['port'])) {
+    $dbport = $urlParts['port'];
+}
+$dbport = (int) ($dbport ?: 3306);
 
 // Prevent uncaught mysqli exceptions so we can show a clear deploy-time error.
 if (!function_exists('mysqli_connect')) {
