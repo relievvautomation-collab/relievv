567a6fbc39881e8152e5150c6f17d83865bb31f4
Relievv Automation <relievvautomation@gmail.com>
2026-03-24 14:47:11 +0530

    Delete init-db.sh

--- init-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Waiting for MySQL to be ready..."
-until mysqladmin ping -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p"$DB_PASS" --silent; do
-    sleep 2
-done
-
-echo "Importing database..."
-mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" < /var/www/html/relievv.sql
-echo "Database imported successfully!"
