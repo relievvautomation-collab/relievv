dee510eab3e1768ac892dcda00ccba2bcc4265a5
Relievv Automation <relievvautomation@gmail.com>
2026-04-15 12:39:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -268,6 +268,21 @@
             <p class="section-subtitle">Automate boring manual tasks in finance, audit, and accounts. More powerful tools coming soon!</p>
             
             <div class="row gy-3">
+                 <div class="col-md-6 col-lg-3">
+                    <div class="tool-card" id="tool-merge">
+                        <div class="tool-icon">
+                           <i class="fa-solid fa-file-pdf"></i>
+                        </div>
+                        <h5>PDF to Excel</h5>
+                        <p>Convert PDF files into clean, structured, and editable Excel sheets in seconds. Extract tables, statements, invoices, and reports with high accuracy and ready-to-use formatting.</p>
+                       
+                              <button class="btn-use-tool"
+                            onclick="windowredirecturl('https://pdftoexcel.relievv.in/', '_blank')">
+                            Use Tool
+                            </button>
+                     
+                    </div>
+                </div>
                 <div class="col-md-6 col-lg-3">
                     <div class="tool-card" id="tool-merge">
                         <div class="tool-icon">
